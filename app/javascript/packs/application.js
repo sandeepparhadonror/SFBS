e21a79e1bfe0e3d01e995e196b1c3b04c465c4b3
sandeepparhadonror <sandeepparhadonror@gmail.com>
2020-04-07 17:02:18 +0530

    added-google-translator-js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,38 @@ document.addEventListener("turbolinks:load", () => {
 })
 
 
+//Add Goole Translator API JS call
+google.load("elements", "1", {
+	packages: "transliteration"
+});
+
+function onLoad() {
+	var options = {
+		sourceLanguage:
+		google.elements.transliteration.LanguageCode.ENGLISH,
+		destinationLanguage:
+		[google.elements.transliteration.LanguageCode.MARATHI],
+		shortcutKey: 'ctrl+g',
+		transliterationEnabled: true
+	};
+
+        // Create an instance on TransliterationControl with the required
+        // options.
+        var control =
+        new google.elements.transliteration.TransliterationControl(options);
+
+        // Enable transliteration in the textbox with id
+        // 'transliterateTextarea'.
+        var transliterationInputIds = ['farmer_detail_first_name', 'farmer_detail_middle_name', 'farmer_detail_last_name', 
+        'farmer_detail_phone_number', 'farmer_detail_transportation_vehicle', 'farmer_detail_address']
+        control.makeTransliteratable(transliterationInputIds);
+      }
+      google.setOnLoadCallback(onLoad);
+
+////////////////
+
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
